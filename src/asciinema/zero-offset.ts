3a572025b9e801a64019cc3af59ba4654035a3c0
Aidan Bleser <aidanbleser35@gmail.com>
2025-02-08 18:22:28 -0600

    add offset script

---
+++ src/asciinema/zero-offset.ts
@@ -0,0 +1,37 @@
+import fs from "node:fs";
+
+const args = process.argv.slice(2);
+
+const originalFile = args[0];
+const newFile = args[1];
+
+const file = fs.readFileSync(originalFile).toString();
+
+const lines = file.split("\n");
+
+let offsetBy = 0;
+
+for (let i = 0; i < lines.length; i++) {
+	if (i === 0) continue;
+	if (lines[i].trim() === "") continue;
+
+	if (i === 1) {
+		offsetBy = parseTime(lines[i]);
+	}
+
+	lines[i] = editTime(lines[i], offsetBy);
+}
+
+fs.writeFileSync(newFile, lines.join("\n"));
+
+function editTime(line: string, offset: number) {
+	const time = parseTime(line);
+
+	return `[${time - offset},${line.slice(2 + time.toString().length)}`;
+}
+
+function parseTime(line: string) {
+	const comma = line.indexOf(",");
+
+	return parseFloat(line.slice(1, comma));
+}
